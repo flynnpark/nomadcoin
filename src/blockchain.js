9bae4050d992a809cdab5a653787b981eca520db
Flynn <wphestiraid@gmail.com>
2018-05-08 12:37:20 +0900

    Creating the Genesis Block
    - Genesis Block은 모든 블록체인이 시작되는 곳
    - Genesis Block은 하드 코딩이 되어야 함.

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,15 @@ class Block {
     this.data = data;
   }
 }
+
+const genesisBlock = new Block(
+  0,
+  'B326BD92BE843EFB8240544CB382CC9BB628A870939BC7D21AD90E8998141E64',
+  null,
+  1525750421,
+  'This is the Genesis Block!'
+);
+
+let blockchain = [genesisBlock];
+
+console.log(blockchain);
