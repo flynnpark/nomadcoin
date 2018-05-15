7af2e5875df3f6159ec9de37136b65d432b1c765
Flynn <wphestiraid@gmail.com>
2018-05-15 20:22:02 +0900

    Realtime Broadcasting of New Found Blocks
    - 새로운 블록을 생성하면 모든 피어들이 새로운 블록을 요청하도록 브로드캐스팅
    - 다른 피어들은 새로운 블록을 요청함
    - 새로운 블록이 생기면 모든 피어가 동기화

--- src/p2p.js
+++ src/p2p.js
@@ -103,7 +103,9 @@ const handleBlockchainResponse = receivedBlocks => {
   const newestBlock = getNewestBlock();
   if (latestBlockReceived.index > newestBlock.index) {
     if (newestBlock.hash === latestBlockReceived.previousHash) {
-      addBlockToChain(latestBlockReceived);
+      if (addBlockToChain(latestBlockReceived)) {
+        broadcastNewBlock();
+      }
     } else if (receivedBlocks.length === 1) {
       sendMessageToAll(getAll());
     } else {
@@ -120,6 +122,8 @@ const sendMessageToAll = message =>
 const responseLatest = () => blockchainResponse([getNewestBlock()]);
 
 const responseAll = () => blockchainResponse(getBlockchain());
+
+const broadcastNewBlock = () => sendMessageToAll(responseLatest());
 
 const handleSocketError = ws => {
   const closeSocketConnection = ws => {
@@ -139,5 +143,6 @@ const connectToPeers = newPeer => {
 
 module.exports = {
   startP2PServer,
-  connectToPeers
+  connectToPeers,
+  broadcastNewBlock
 };
