961565986906abee8e6189d83391f6fd96a2d5b2
Flynn <wphestiraid@gmail.com>
2018-05-24 20:33:49 +0900

    Checking the Referenced Address
    - 지갑 주소 유효성 검증

--- src/transaction.js
+++ src/transaction.js
@@ -60,9 +60,20 @@ const signTxIn = (tx, txInIndex, privateKey, uTxOut) => {
     console.log(`Couldn't find the referenced uTxOut, not signing`);
     return;
   }
+  const referencedAddress = referencedUTxOut.address;
+  if (getPublicKey(privateKey) !== referencedAddress) {
+    return;
+  }
   const key = ec.keyFromPrivate(privateKey, 'hex');
   const signature = Utils.toHexString(key.sign(dataToSign).toDER());
   return signature;
+};
+
+const getPublicKey = privateKey => {
+  return ec
+    .keyFromPrivate(privateKey, 'hex')
+    .getPublic()
+    .encode('hex');
 };
 
 const updateUTxOuts = (newTxs, uTxOutList) => {
