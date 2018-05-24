d2b3215426817b7710788aa05e84d9d518c061f9
Flynn <wphestiraid@gmail.com>
2018-05-24 20:28:05 +0900

    Getting Address and Address Balance

--- src/wallet.js
+++ src/wallet.js
@@ -1,6 +1,7 @@
 const Elliptic = require('elliptic');
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
 
 const ec = new Elliptic.ec('secp256k1');
 
@@ -10,6 +11,24 @@ const generatePrivateKey = () => {
   const keyPair = ec.genKeyPair();
   const privateKey = keyPair.getPrivate();
   return privateKey.toString(16);
+};
+
+const getPrivateFromWallet = () => {
+  const buffer = fs.readFileSync(privateKeyLocation, 'utf-8');
+  buffer.toString();
+};
+
+const getPublicFromWallet = () => {
+  const privateKey = getPrivateFromWallet();
+  const key = ec.keyFromPrivate(privateKey, 'hex');
+  return key.getPublic().encode('hex');
+};
+
+const getBalance = (address, uTxOuts) => {
+  return _(uTxOuts)
+    .filter(uTxOut => uTxOut.address === address)
+    .map(uTxOut => uTxOut.amount)
+    .sum();
 };
 
 const initWallet = () => {
