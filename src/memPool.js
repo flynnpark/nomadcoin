d005a3a3019083992e2217af275d1896030c9cd6
Flynn <wphestiraid@gmail.com>
2018-06-02 20:53:12 +0900

    Validating Transactions for MemPool
    - memPool을 위해 트랜잭션이 유효한지 검증
    - 새로운 트랜잭션이 memPool 안에 중복되는 인풋이 있는지 검증

--- src/memPool.js
+++ src/memPool.js
@@ -4,3 +4,28 @@ const Transactions = require('./transactions');
 const { validateTx } = Transactions;
 
 let momPool = [];
+
+const getTxInsInPool = memPool =>
+  _(memPool)
+    .map(tx => tx.txIns)
+    .flatten()
+    .value();
+
+const isTxValidForPool = (tx, memPool) => {
+  const txInsInPool = getTxInsInPool(memPool);
+
+  const isTxInAlreadyInPool = (txIns, txIn) =>
+    _.find(txIns, txInInPool => {
+      return (
+        txIn.txOutIndex === txInInPool.txOutIndex &&
+        txIn.txOutId === txInInPool.txOutId
+      );
+    });
+
+  for (const txIn of tx.txIns) {
+    if (isTxInAlreadyInPool(txInsInPool, txIn)) {
+      return false;
+    }
+  }
+  return true;
+};
