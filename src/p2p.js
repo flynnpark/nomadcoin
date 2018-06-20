a7b05e3c5ce987bd45b57f1b3ba96a47a8882d0d
Flynn <wphestiraid@gmail.com>
2018-06-20 21:05:58 +0900

    Fix typo
    - 오류 수정

--- src/p2p.js
+++ src/p2p.js
@@ -78,11 +78,11 @@ const initSocketConnection = ws => {
   handleSocketError(ws);
   sendMessage(ws, getLatest());
   setTimeout(() => {
-    sendMessageToAll(ws, getAllMempool());
+    sendMessageToAll(getAllMempool());
   }, 1000);
   setInterval(() => {
     if (sockets.includes(ws)) {
-      sendMessage(ws);
+      sendMessage(ws, '');
     }
   }, 1000);
 };
